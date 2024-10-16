f2bde112b29581d31a154475248b4ba8c493dcff
NaveenRam <accessnaveen@gmail.com>
2024-10-16 11:44:03 -0500

    Initial Commit

---
+++ ShimmeringText/ShimmeringTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmeringTextApp.swift
+//  ShimmeringText
+//
+//  Created by Naveen Ramachandrappa on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmeringTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
